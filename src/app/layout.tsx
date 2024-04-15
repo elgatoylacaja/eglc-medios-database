2f7d28f718ab4eac39288d3fb2936ecac6eab4b5
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2024-04-15 09:34:01 -0300

    update fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,18 @@
+import { Montserrat, Overpass_Mono } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  display: "swap",
+  subsets: ["latin"],
+});
+
+const overpass_mono = Overpass_Mono({
+  variable: "--font-overpass-mono",
+  display: "swap",
+  subsets: ["latin"],
+});
 
 export default async function RootLayout({
   children,
@@ -9,7 +22,11 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-        <main className="p-6">{children}</main>
+        <main
+          className={`p-6 ${montserrat.variable} ${overpass_mono.variable} font-sans`}
+        >
+          {children}
+        </main>
         <Toaster />
       </body>
     </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-montserrat)"],
+        mono: ["var(--font-overpass-mono)"],
+      },
+    },
   },
   plugins: [],
 };
