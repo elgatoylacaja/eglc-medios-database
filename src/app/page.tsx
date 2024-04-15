57c203e8c42fe95a0104559d46e8a43784554884
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2024-04-15 09:34:24 -0300

    remove unused files

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import prisma from "../lib/prisma";
-import ChannelList from "../views/ChannelsList";
-
-export default async function Home() {
-  const channels = await prisma.channel.findMany({});
-  return <ChannelList channels={channels} />;
-}
